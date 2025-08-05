66e49883a29577cf17415605ae43f4c1a4fac24c
yanikem <yanikem@gmail.com>
2025-08-05 17:51:19 +0000

    pwa app

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,40 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+    import('vite-plugin-pwa').then(({ VitePWA }) => VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'LinkedBi.o',
+        short_name: 'LinkedBi.o',
+        description: 'Your corner of the internet! Create your own link in bio page.',
+        theme_color: '#FFFFFF',
+        background_color: '#FFFFFF',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/linkedbio_logo.jpg',
+            sizes: '192x192',
+            type: 'image/jpeg',
+          },
+          {
+            src: '/linkedbio_logo.jpg',
+            sizes: '512x512',
+            type: 'image/jpeg',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    })),
+  ],
 });
